db9ec371f4e450f6bdec8cce9d8fc849594082f2
Shivangi Agrawal <shivangiagrawal667@gmail.com>
2021-01-13 17:21:43 +0530

    Inserted file trainingtheset.py

---
+++ traningtheset.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+import os
+from PIL import Image
+#recognizer=cv2.createLBPHFaceRecognizer()
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+path='dataset'\
+
+def getImageWithID(path):
+    imagePaths=[os.path.join(path,f) for f in os.listdir(path)]
+    faces=[]
+    IDs=[]
+    for imagePath in imagePaths:
+        faceImg=Image.open(imagePath).convert('L')
+        faceNp=np.array(faceImg,'uint8')
+        ID=int(os.path.split(imagePath)[-1].split('.')[1])
+        faces.append(faceNp)
+        IDs.append(ID)
+        cv2.imshow("training",faceNp)
+        cv2.waitKey(10)
+    return np.array(IDs), faces
+Ids, faces=getImageWithID(path)
+recognizer.train(faces,Ids)
+recognizer.save('recognizer/trainingData.yml')
+cv2.destroyAllWindows()
